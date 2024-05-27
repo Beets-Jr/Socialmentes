7d5efe7e5ce497a56efe26de7fac5853273f7994
Pedro Correia <peeucorreiabr@gmail.com>
2024-05-27 10:27:18 -0300

    Versão final (remover os trechos de teste)

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,55 +1,105 @@
 import { Link } from 'react-router-dom'
 import { auth } from '../../Database/FirebaseConfig.mjs'
 
-import { AppBar, Toolbar, Typography, Box } from '@mui/material'
+import { AppBar, Toolbar, Typography, Box, IconButton, Avatar } from '@mui/material'
 import { PiUserCircleFill } from 'react-icons/pi'
 import LogoutIcon from './Icons/LogoutIcon'
 
+function stringAvatar(name){
+
+  const nameParts = name.split(' ');
+  const initials = nameParts.length > 1 ? `${nameParts[0][0]}${nameParts[1][0]}` : `${nameParts[0][0]}`;
+
+  return {
+    sx:{
+      bgcolor: '#FFFFFF',
+    },
+    children: initials,
+  };
+
+}
+
 function Header({title}) {
 
+  // Recupera o usuário que está logado
   const user = auth.currentUser
 
+  // Usuário para teste e estilização
+  // const user = {
+  //   fullName:'João da Silva',
+  //   photo: 'src/Components/Header/Icons/fotoTeste.png'
+  // }
+
+  // Usuário sem foto para teste
+  // const user = {
+  //   fullName: 'José Antônio',
+  //   photo: null
+  // }
+
+  function logout(){
+    auth.signOut()
+  }
+
   return (
-    <AppBar position='static' sx={{background: '#FFFFFF', width: '80vw', marginLeft: '20vw'}}>
+    <AppBar position='static' sx={{background: '#FFFFFF', width: '80vw', marginLeft: '20vw', borderRadius: '0 0 15px 15px', height: '15vh', display: 'flex', justifyContent: 'center'}}>
       <Toolbar sx={{display: 'flex', justifyContent: 'space-between'}}>
 
+        {/* Título do Header */}
         <Typography
           variant='h4'
           component='div'
-          sx={{fontFamily: 'var(--font-text)', color: 'var(--color-blue-4)', marginLeft: '50px'}}>
+          sx={{fontFamily: 'var(--font-title)', color: 'var(--color-blue-4)', marginLeft: '50px'}}>
           {title}
         </Typography>
             
+        {/* Se o usuário existir deve ser exibido o seu nome e sua foto, caso contrário, deve-se exibir uma opção para o login*/}
         {user ? 
-          <Box sx={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-            <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
-              {user.fullName}
+          <Box sx={{display: 'flex', flexDirection: 'row', alignItems: 'center', gap: '1.5vw'}}>
+
+            {/* Caixa de texto para quando o usuário está logado */}
+            <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>         
               <Typography
-              variant='h6'
-              component={Link}
-              to = '/conta'
-              sx={{fontFamily: 'var(--font-text)', color: 'var(--color-blue-3)', textDecoration: 'none'}}>
+                variant='subtitle2'
+                component='div'
+                sx={{fontFamily: 'var(--font-title)', color: 'var(--color-blue-2)'}}>
+              {user.fullName}
+              </Typography>
+
+              <Typography
+                variant='subtitle2'
+                component={Link}
+                to = '/conta'
+                sx={{fontFamily: 'var(--font-text)', color: 'var(--color-blue-1)', textDecoration: 'none'}}>
               Editar Perfil
               </Typography>
             </Box>
+
+            {/* Exibe a foto do usuário caso ela exista ou um avatar com as suas iniciais caso não exista */}
             {user.photo ? 
-              <img src={user.photo} alt='Foto do usuário'/> :
-              <div>
-                {/* Lógica do avatar */}
-              </div>
+              <Avatar src={user.photo} sx={{width: '50px', height: '50px', border: '2.5px solid var(--color-blue-1)'}}></Avatar>
+              :
+              <Avatar {...stringAvatar(user.fullName)} sx={{width: '50px', height: '50px', border: '2.5px solid var(--color-blue-1)'}}></Avatar>
             }
-            {/* <LogoutIcon/> */}
+
+            {/* Botão de logout */}
+            <IconButton onClick={logout()}>
+              <LogoutIcon/>
+            </IconButton>
           </Box>
           :
           <Box sx={{display:'flex', flexDirection:'row', alignItems: 'center', gap:'10px'}}>
+
+            {/* Texto que redireciona para o login se o usuário estiver deslogado */}
             <Typography
-              variant='h6'
+              variant='h5'
               component={Link}
               to = '/login'
               sx={{fontFamily: 'var(--font-text)', color: 'var(--color-blue-3)', textDecoration: 'none'}}>
             Login
             </Typography>
-            <PiUserCircleFill style={{color:'var(--color-blue-3)', width:'40px', height:'40px'}}/>
+
+            <PiUserCircleFill style={{color:'var(--color-blue-3)', width:'50px', height:'50px'}}/>
+
           </Box>
         }
 
