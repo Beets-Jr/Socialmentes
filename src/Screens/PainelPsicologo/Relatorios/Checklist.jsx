ac7f4a8b2ae923c271608c9d5c04a7f92d886db9
Laura Rieko Marcal Imai <laura.imai@estudante.ufscar.br>
2024-06-27 16:41:41 -0300

    Adding patient data component

--- src/Screens/PainelPsicologo/Relatorios/Checklist.jsx
+++ src/Screens/PainelPsicologo/Relatorios/Checklist.jsx
@@ -1,13 +1,29 @@
+import { Typography, Box, Stack } from "@mui/material";
 import ChecklistItem from "../../../Components/PainelPsicologo/Relatorios/ChecklistComponent/ChecklistItem";
 import ReportBtn from "../../../Components/PainelPsicologo/Relatorios/ChecklistComponent/ReportBtn";
+import PacientData from "../../../Components/PainelPsicologo/Relatorios/ChecklistComponent/PatientData"
+import styles from "./Checklist.module.css";
 
-function Checklist() {
+function Checklist({name, birthday}) {
     return(
         <div>
-            <ReportBtn name="Gráfico" path="/"></ReportBtn> {/*Colocar o path certo*/}
-            <ReportBtn name="Tabela" path="" />
-            <ReportBtn name="Relatório" path="" />
+            <Typography variant="h4" className={styles.checklistTitle}>
+                Checklist Currículo Modelo de Intervenção Precoce Finalizado
+            </Typography>
             
+            <Box className={styles.menuContainer}>
+                <PacientData name={name} birthday={birthday}/>
+                <Stack direction="row" spacing={2}>
+                    <ReportBtn name="Gráfico" path="/"></ReportBtn> {/*Colocar o path certo*/}
+                    <ReportBtn name="Tabela" path="" />
+                    <ReportBtn name="Relatório" path="" />
+                </Stack>
+            </Box>
+
+            <Typography variant="h5" className={styles.checklistTitle}>
+                Comunicação Receptiva - Nível 1
+            </Typography>
+
             <ChecklistItem
                 index={1}
                 hability="Habilidade A"
