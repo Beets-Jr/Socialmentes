077803c15e5900db0c762cdeab2fb12c3450742a
vinciuscastro <vinisilvacastro71@gmail.com>
2024-07-03 14:01:51 -0300

    refactor: Update InterventionTeams component to fetch user profiles and populate select box options

--- src/Components/PainelAdm/PatientRegistration/forms/InterventionTeams.jsx
+++ src/Components/PainelAdm/PatientRegistration/forms/InterventionTeams.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import SSelectBox from '../SSelectBox';
 import AddButton from '../AddButton';
 import SDividerInt from '../SDividerInt';
-
-const teamAccompanimentOptions = ['Fonoaudiologia', 'Psicologia', 'Psicopedagogia', 'Terapia Ocupacional', 'Fisioterapia', 'Nutrição'];
+import { UserService } from '../../../../Services/userService';
+import { CircularProgress } from '@mui/material';
 
 const InterventionTeams = ({ values, setValues, handleArrayChange, error }) => {
+  const [userProfiles, setUserProfiles] = useState([]);
+  const [options, setOptions] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    UserService.getAllUsers()
+      .then((value) => {
+        setUserProfiles(value);
+        setIsLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      const updatedOptions = userProfiles.map(user => `${user.fullName} (${user.specialization})`);
+      setOptions(updatedOptions);
+    }
+  }, [isLoading, userProfiles]);
+
   const handleAddInterventionTeam = () => {
     setValues({
       ...values,
@@ -16,26 +35,36 @@ const InterventionTeams = ({ values, setValues, handleArrayChange, error }) => {
     });
   };
 
+  const renderSelectBox = (team, index) => {
+    const isValidValue = options.includes(team.name);
+    return (
+      <SSelectBox
+        xs={11.4}
+        name={`interventionTeams[${index}].name`}
+        label="Equipe intervenção"
+        handleChange={(e) => handleArrayChange(index, 'interventionTeams', 'name', e.target.value)}
+        value={isValidValue ? team.name : ''}
+        options={options}
+        error={error?.interventionTeams?.[index]?.name}
+      />
+    );
+  };
+
   return (
     <>
-      {
+      {isLoading ? (
+        <CircularProgress />
+      ) : (
         values.interventionTeams.map((team, index) => (
           <React.Fragment key={'team' + index}>
-            <SSelectBox xs={11.4}
-              name={`interventionTeams[${index}].name`}
-              label="Equipe intervenção"
-              handleChange={(e) => handleArrayChange(index, 'interventionTeams', 'name', e.target.value)}
-              value={team.name}
-              options={teamAccompanimentOptions}
-              error={error?.interventionTeams?.[index]?.name}
-            />
+            {renderSelectBox(team, index)}
             <AddButton handleClick={handleAddInterventionTeam} />
           </React.Fragment>
         ))
-      }
+      )}
       <SDividerInt />
+    </>
+  );
+};
 
-    </>);
-}
-
-export default InterventionTeams
+export default InterventionTeams;
