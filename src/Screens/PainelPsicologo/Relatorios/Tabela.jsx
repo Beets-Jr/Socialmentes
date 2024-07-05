d2d9f2d71614e4004693f7b3f27ef3a6aa296841
mutannejs <murillojustinoklein@gmail.com>
2024-07-04 23:31:07 -0300

    style: tabela estilizada

--- src/Screens/PainelPsicologo/Relatorios/Tabela.jsx
+++ src/Screens/PainelPsicologo/Relatorios/Tabela.jsx
@@ -23,17 +23,47 @@ function Tabela() {
     useEffect(() => {
         TestService.getTestById(testId)
             .then( test => {
-                if (test instanceof Error) return;
+                if (test instanceof Error) {
+                    console.log(test.message);
+                    return;
+                }
                 setTest(test);
                 console.log(test);
                 return PatientService.getPatientById(test.patientId)
             })
             .then( data => {
-                if (!data || data instanceof Error) return;
+                if (!data || data instanceof Error) {
+                    console.log(test.message);
+                    return;
+                }
                 setPatient(data);
                 console.log(data);
             });
     }, []);
+
+    const getAge = (date) => {
+        if (!date) return;
+        const now = new Date();
+        const dataString = date;
+        const dateBirth = new Date(
+            dataString.slice(0, 4),
+            Number(dataString.slice(5, 7)) - 1,
+            dataString.slice(8, 10)
+        );
+        const years = now.getFullYear() - dateBirth.getFullYear();
+        const months = now.getMonth() - dateBirth.getMonth();
+        const days = now.getDate() - dateBirth.getDate();
+
+        if (months < 0) {
+            return `${years - 1} anos e ${12 + months} mes(es)`;
+        } else if (months > 0) {
+            return `${years} anos e ${months} mes(es)`;
+        } else if (days < 0) {
+            return `${years - 1} anos e ${months > 0 ? months - 1 : 11} mes(es)`;
+        } else {
+            return `${years} anos`;
+        }
+    }
 
     const [isLoading, setIsLoading] = useState(false);//(true);
 
@@ -47,48 +77,39 @@ function Tabela() {
             ) : (
                 <Box>
                     <Typography className={styles.user_text}>
-                        <Typography component='span'>Nome:</Typography> {user.nome}
+                        <Typography component='span'>Nome:</Typography> {patient?.children.name}
                     </Typography>
                     <Typography className={styles.user_text}>
-                        <Typography component='span'>Idade:</Typography> {user.age}
+                        <Typography component='span'>Idade:</Typography> {getAge(patient?.children.dateBirth)}
                     </Typography>
                     <Typography className={styles.legend} display='flex' alignItems='center'>
                         <Check fontSize="inherit"/>&nbsp;Adquirido&nbsp;|&nbsp;<Close fontSize="inherit" />&nbsp;Não avaliado
                     </Typography>
 
-                    <TableContainer className={styles.table_container}>
-                        <Table>
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell>Domínio</TableCell>
-                                    <TableCell>Nível 1 (1a - 1a 6m)</TableCell>
-                                    <TableCell>Nível 2 (1a 6m - 2a)</TableCell>
-                                    <TableCell>Nível 3 (2a - 3a)</TableCell>
-                                    <TableCell>Nível 4 (3a - 4a)</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                <TableRow>
-                                    <TableCell>Comunicação Receptiva</TableCell>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCell>Comunicação Expressiva</TableCell>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCell>Competências Socias:<br/>Adultos e Pares</TableCell>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCell>Cognição</TableCell>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCell>Jogo</TableCell>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCell>Competências Sociais</TableCell>
-                                </TableRow>
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
+                        <Box className={styles.table_border}>
+                        <TableContainer className={styles.table_container}>
+                            <Table>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Domínio</TableCell>
+                                        <TableCell>Nível 1 (1a - 1a 6m)</TableCell>
+                                        <TableCell>Nível 2 (1a 6m - 2a)</TableCell>
+                                        <TableCell>Nível 3 (2a - 3a)</TableCell>
+                                        <TableCell>Nível 4 (3a - 4a)</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    { Object.keys(test.questions).map( level => {
+                                        <TableRow key={level}>
+                                            <TableCell>{level}</TableCell>
+                                            <TableCell><Check fontSize="small"/></TableCell>
+                                            <TableCell><Close fontSize="small" /></TableCell>
+                                        </TableRow>
+                                    })}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </Box>
                 </Box>
             )}
 
