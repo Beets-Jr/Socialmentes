d085528117a3e4a95fcb0d52eb3bd32753633a9b
Laura Rieko Marcal Imai <laura.imai@estudante.ufscar.br>
2024-07-12 18:18:18 -0300

    Adding changes to the button and the data display

--- src/Components/PainelPsicologo/HomeDataCard.jsx
+++ src/Components/PainelPsicologo/HomeDataCard.jsx
@@ -1,13 +1,12 @@
-import { Grid, Card, Typography, Box, Divider, Button} from '@mui/material';
+import * as React from 'react';
+import { Grid, Card, Typography, Box, Divider, Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 function formatTimestamp(timestamp) {
   const date = new Date(timestamp);
-
   const day = String(date.getDate()).padStart(2, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0'); 
+  const month = String(date.getMonth() + 1).padStart(2, '0');
   const year = date.getFullYear();
-
   return `${day}/${month}/${year}`;
 }
 
@@ -16,11 +15,11 @@ function calcularIdade(dataNascimento) {
   let dia, mes, ano;
 
   if (dataNascimento.includes('/')) {
-      [dia, mes, ano] = dataNascimento.split('/').map(Number);
+    [dia, mes, ano] = dataNascimento.split('/').map(Number);
   } else if (dataNascimento.includes('-')) {
-      [ano, mes, dia] = dataNascimento.split('-').map(Number);
+    [ano, mes, dia] = dataNascimento.split('-').map(Number);
   } else {
-      return 'Formato de data inválido';
+    return 'Formato de data inválido';
   }
   
   const nascimento = new Date(ano, mes - 1, dia);
@@ -29,8 +28,8 @@ function calcularIdade(dataNascimento) {
   let idadeMeses = hoje.getMonth() - nascimento.getMonth();
 
   if (idadeMeses < 0 || (idadeMeses === 0 && hoje.getDate() < nascimento.getDate())) {
-      idadeAnos--;
-      idadeMeses = 12 + idadeMeses;
+    idadeAnos--;
+    idadeMeses = 12 + idadeMeses;
   }
 
   return `Idade: ${idadeAnos} ano(s) e ${idadeMeses} mês(es)`;
@@ -38,102 +37,108 @@ function calcularIdade(dataNascimento) {
 
 const HomeDataCard = ({ title, data, type }) => {
   const renderItem = (item) => {
-      if (type === 'patient') {
-        return (
-          <Typography 
-            sx={{
-              fontFamily:'var(--font-text)', 
-              color:'var(--color-gray-4)',
-              textAlign: 'center'
-            }}
-          >
-            {item.children.name} | {calcularIdade(item.children.dateBirth)} | Não tem timestamp 
-          </Typography>
-        );
-      } else {
-        return (
-          <Typography 
-            sx={{
-              fontFamily:'var(--font-text)', 
-              color:'var(--color-gray-4)',
-              textAlign: 'center'
-            }}
-          >
-            {item.id} | {item.testType} | {item.patientName} | {formatTimestamp(item.timestamp)} 
-          </Typography>
-        );
-      }
+    if (type === 'patient') {
+      return (
+        <Typography 
+          sx={{
+            fontFamily: 'var(--font-text)', 
+            color: 'var(--color-gray-4)',
+            textAlign: 'center'
+          }}
+        >
+          {item.children.name} | {calcularIdade(item.children.dateBirth)} | Não tem timestamp 
+        </Typography>
+      );
+    } else {
+      return (
+        <Typography 
+          sx={{
+            fontFamily: 'var(--font-text)', 
+            color: 'var(--color-gray-4)',
+            textAlign: 'center'
+          }}
+        >
+          {item.id} | {item.testType} | {item.patientName} | {formatTimestamp(item.timestamp)} 
+        </Typography>
+      );
+    }
   };
 
   const navigate = useNavigate();
   const handleClick = () => {
-      navigate('/'); // Colocar o caminho certo
+    navigate('/'); // Colocar o caminho correto
   };
-    
 
   return (
     <Grid item xs={12} md={6}>
-        <Card variant="outlined" 
+      <Card variant="outlined" 
+        sx={{
+          border: '2px solid #e0e0e0', 
+          borderRadius: '10px', 
+          height: '30vh', 
+          display: 'flex', 
+          flexDirection: 'column', 
+          justifyContent: 'space-between',
+          padding: '10px', 
+          width: '100%',
+          height: '420px',
+          borderRadius: '11px',
+          border: '2px solid #E0E0E0'
+        }}
+      >
+        <Typography 
+          variant="h6"
           sx={{
-            border:'2px solid #e0e0e0', 
-            borderRadius:'10px', 
-            height:'35vh', 
-            display:'flex', 
-            flexDirection:'column', 
-            justifyContent:'space-between',
-            padding:'5px'
-          }}
+            fontFamily: 'var(--font-text)', 
+            color: 'var(--color-gray-5)',
+            display: 'flex',
+            justifyContent: 'center', 
+            marginTop: '5px', 
+            fontSize: { xs: '20px', sm: '24px', md: '28px' },
+            fontWeight: '400',
+            textAlign: 'center'
+          }} 
+        >
+          Últimos {title}
+        </Typography>
+        <Divider variant="middle" sx={{ backgroundColor: 'var(--color-gray-5)', height: '2px' }} />
+        <Box >
+          {data.map((item, index) => (
+            <Box key={index} sx={{ width: '100%', textAlign: 'center', my: '30px' }}>
+              {renderItem(item)}
+              <Divider variant="middle" sx={{ backgroundColor: 'var(--color-gray-4)', height: '1px', marginY: 1 }} />
+            </Box>
+          ))}
+        </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end', marginBottom:'5px' }}>
+          <Button 
+            variant="outlined" 
+            disableElevation 
+            onClick={handleClick} 
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              fontFamily: 'var(--font-text)',
+              color: 'var(--color-gray-4)',
+              margin: '5px', 
+              padding: '5px 35px',
+              borderRadius: '7px', 
+              borderWidth: '2px',
+              borderColor: 'var(--color-gray-3)',
+              textTransform: 'none',
+              '&:hover': {
+                color: '#fff',
+                borderWidth: '2px',
+                borderColor: '#65AAEA',
+                background: 'linear-gradient(to left, var(--color-blue-3), var(--color-blue-2))',
+              }              
+            }}
           >
-            <Typography 
-              variant="h6"
-              sx={{
-                fontFamily:'var(--font-text)', 
-                color:'var(--color-gray-5)',
-                display:'flex',
-                justifyContent:'center', 
-                marginTop:'5px', 
-                fontSize:{xs: '20px', sm: '24px', md: '28px'},
-                fontWeight:'400',
-                textAlign: 'center'
-              }} 
-            >
-                Últimos {title}
-            </Typography>
-            <Divider variant="middle" sx={{backgroundColor:'var(--color-gray-5)', height:'2px'}}/>
-            <Box sx={{width:'100%', textAlign: 'center', display:'flex', flexDirection:'column', 
-            justifyContent:'space-between',}}>
-              {data.map(item => (
-                  <Box >
-                      {renderItem(item)}
-                      <Divider variant="middle" sx={{backgroundColor:'var(--color-gray-4)', height: '1px', marginY: 1}} />
-                  </Box>
-              ))}
-            </Box>
-            <Box sx={{display:'flex', justifyContent:'flex-end'}}>
-              <Button 
-                variant="outlined" 
-                disableElevation 
-                onClick={handleClick} 
-                sx={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  fontFamily: 'var(--font-text)',
-                  color: 'var(--color-gray-4)',
-                  borderColor: '2px solid var(--color-gray-3)',
-                  margin: '5px', padding: '0 auto',
-                  borderRadius: '10px', 
-                  '&:hover': {
-                    color: '#fff',
-                    borderColor: '2px solid var(--color-blue-3)',
-                    background: 'linear-gradient(to left, var(--color-blue-3), var(--color-blue-2))',
-                  }              
-                }}
-            >
-                Ver mais
-            </Button>
-            </Box>
-        </Card>
+            Ver mais
+          </Button>
+        </Box>
+      </Card>
     </Grid>
   );
 };
