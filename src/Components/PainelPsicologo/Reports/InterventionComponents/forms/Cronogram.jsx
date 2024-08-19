66b0b426603e367229d6ca1b8e17227d319d05a1
vinciuscastro <vinisilvacastro71@gmail.com>
2024-08-19 10:56:50 -0300

    feat: Add user profiles to cronogram form dropdown

--- src/Components/PainelPsicologo/Reports/InterventionComponents/forms/Cronogram.jsx
+++ src/Components/PainelPsicologo/Reports/InterventionComponents/forms/Cronogram.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import StyledInputText from '../StyledInputText';
 import { Grid } from '@mui/material';
 import AddFieldButton from '../AddFieldButton';
 import StyledTitle from '../StyledTitle';
 import StyledCheckBox from '../StyledCheckBox';
 import StyledDivider from '../StyledDivider';
+import { UserService } from '../../../../../Services/userService';
+import { CircularProgress } from '@mui/material';
+import SSelectBox from '../../../../PainelAdm/PatientRegistration/SSelectBox';
+
+
 
 
 const Cronogram = ({ values, setValues, handleMapChange, error }) => {
+  const [userProfiles, setUserProfiles] = useState([]);
+  const [options, setOptions] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    UserService.getAllUsers()
+      .then((value) => {
+        setUserProfiles(value);
+        setIsLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      const updatedOptions = userProfiles.map(user => [`${user.fullName} (${user.specialization})`, user.uid]);
+      setOptions(updatedOptions);
+    }
+  }, [isLoading, userProfiles]);
+
   const handleAddCronogram = () => {
     setValues({
       ...values,
@@ -42,19 +66,30 @@ const Cronogram = ({ values, setValues, handleMapChange, error }) => {
               label="Hora"
               handleChange={(e) => handleMapChange(index, 'cronogram', 'hour', e.target.value)}
               value={cronogram.hour}
-              error={error?.cronogram?.[index]?.hour}
+              error={error?.[`cronogram[${index}].hour`]}
             />
 
-            <StyledInputText lg={4.5}
+            {/* <StyledInputText lg={4.5}
               name={`cronogram[${index}].professional`}
               label="Terapeuta"
               handleChange={(e) => handleMapChange(index, 'cronogram', 'professional', e.target.value)}
               value={cronogram.professional}
               error={error?.cronogram?.[index]?.professional}
+            /> */}
+
+            <SSelectBox
+              lg={4.5}
+              name={`cronogram[${index}].professional`}
+              label="Terapeuta"
+              handleChange={(e) => handleMapChange(index, 'cronogram', 'professional', e.target.value)}
+              value={cronogram.professional}
+              options={options}
+              useCustomOptions
+              error={error?.[`cronogram[${index}].professional`]}
             />
 
             <Grid item lg={5.4} xs={9.5} md={11}>
-              <StyledCheckBox item={cronogram} index={index} handleDayChange={handleDayChange} />
+              <StyledCheckBox item={cronogram} index={index} handleDayChange={handleDayChange} error={error} />
             </Grid>
 
             <AddFieldButton vin handleClick={handleAddCronogram} />
