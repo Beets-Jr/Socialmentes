203f11dba7fadbd967df500e934af75a849ffce7
Laura Rieko Marcal Imai <laura.imai@estudante.ufscar.br>
2024-08-21 17:06:19 -0300

    Fixing checkboxQuestion style

--- src/Components/PainelPsicologo/Reports/ReportFormComponents/CheckboxQuestion.jsx
+++ src/Components/PainelPsicologo/Reports/ReportFormComponents/CheckboxQuestion.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FormControl, FormLabel, FormGroup, FormControlLabel, Checkbox, Stack } from '@mui/material';
+import { FormControl, FormLabel, FormGroup, FormControlLabel, Checkbox } from '@mui/material';
 import { Controller } from 'react-hook-form';
 
 {/*
@@ -13,28 +13,26 @@ function CheckboxQuestion({ control, name, label, options }) {
     return (
         <FormControl component="fieldset" variant="standard">
             <FormLabel component="legend" sx={{fontFamily:'var(--font-sub)', color:'var(--color-blue-3)'}}>{label}</FormLabel>
-            <FormGroup sx={{display:'flex'}}>
-                <Stack direction="row" spacing={2} sx={{display:'flex'}}>
-                    {options.map((option) => (
-                        <Controller
-                            key={option.value}
-                            name={`${name}.${option.value}`}
-                            control={control}
-                            render={({ field }) => (
-                                <FormControlLabel
-                                    control={
-                                        <Checkbox
-                                            {...field}
-                                            checked={field.value}
-                                        />
-                                    }
-                                    label={`${option.value}. ${option.label}`}
-                                    sx={{fontFamily:'var(--font-text)', color:'var(--color-gray-4)'}}
-                                />
-                            )}
-                        />
-                    ))}
-                </Stack>
+            <FormGroup sx={{display:'flex', flexDirection:'row'}}>
+                {options.map((option) => (
+                    <Controller
+                        key={option.value}
+                        name={`${name}.${option.value}`}
+                        control={control}
+                        render={({ field }) => (
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        {...field}
+                                        checked={field.value}
+                                    />
+                                }
+                                label={`${option.value}. ${option.label}`}
+                                sx={{fontFamily:'var(--font-text)', color:'var(--color-gray-4)'}}
+                            />
+                        )}
+                    />
+                ))}
             </FormGroup>
         </FormControl>
     );
